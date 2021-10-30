004eaa4db510e770efcd697e88736b8362a4ea1b
NiekDoo <77921239+NiekDoo@users.noreply.github.com>
2021-10-30 15:01:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,53 +1,58 @@
-    <!doctype html>
-    <html>
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="widt=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <title>Aanmeldingen</title>
-      <head>
-      <body>
-        <table class="table table-striped table-bordered">
-          <tr>
-            <th>Naam</th>
-            <th>Mail</th>
-            <th>Aanwezig</th>
-            <th>aantal</th>
-          </tr>
-          <?php
-              // Declareer variabelen
-              $servername = "localhost";
-              $username = "root";
-              $password = "usbw";
-              $database = "aanmelding";
+<html>
+    <header>
 
-              // Maak verbinding
-              $conn = new mysqli($servername, $username, $password, $database);
+    </header>
+    <body>
+        <input type="button" value="Tabel met namen" onClick="document.location.href='/index.php'" />
+        <?php
+            // Declareer variabelen
+            $servername = "localhost";
+            $username = "root";
+            $password = "usbw";
+            $database = "aanmelding";
 
-              // Controleer of verbinding is gelukt
-              if ($conn->connect_error) {
-                  die("Connectie mislukt: " . $conn->connect_error);
-              }
-              echo "Connectie gemaakt";
+            // Maak verbinding
+            $conn = new mysqli($servername, $username, $password, $database);
 
-              // Laat tabel zien
-              $sql = "SELECT naam, mail, aanwezig, aantal FROM Aanmelding";
-              $result = $conn->query($sql);
+            // Controleer of verbinding is gelukt
+            if ($conn->connect_error) {
+                die("Connectie mislukt: " . $conn->connect_error);
+            }
+            echo "Connectie gemaakt";
 
-                if ($result->num_rows > 0) {
-                  // output data of each row
-                  while($row = $result->fetch_assoc()) {
-                    echo "<tr><td>" . $row["naam"]. "</td><td>" . $row["mail"] . "</td><td>" . $row["aanwezig"] . "</td><td>" . $row["aantal"] . "<br>";
-                  }
+
+            $sql = "SELECT * \n"
+            . "FROM aanmelding LIMIT 0, 30 ";
+            echo $sql;
+
+            /*
+            $sql = "INSERT INTO aanmelding (naam, mail, aanwezig, aantal) VALUES ('Jan', 'jan@gomail.com', 1, 22)";
+
+                if ($conn->query($sql) === TRUE) {
+                    echo "Jan is succesvol toegevoegd.";
                 } else {
-                  echo "0 results";
+                    echo "Error: " . $sql . "<br>" . $conn->error;
                 }
+            */
 
-              //Sluit verbinding
-              $conn->close();
-              ?>
+            // Gegevens toevoegen met variabelen
+            $stmt = $conn->prepare("INSERT INTO aanmelding (naam, mail) VALUES (?, ?)");
+            $stmt -> bind_param("ss", $naam, $mail);
 
-        </table>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-      </body>
-    </html>
+            // hier ga je pas de variabelen toewijzen en de query uitvoeren, dat kan je meer keren doen.
+
+            $naam = "Mijn Naam";
+            $mail = "email@mijnnaam.nl";
+            $stmt->execute();
+
+            // let op dat je zowel de prepare als de connectie sluit
+            $stmt->close();
+            $conn->close();
+
+
+
+
+            // Verbinding verbreken aan het eind van de code
+        ?>
+    </body>
+</html>
