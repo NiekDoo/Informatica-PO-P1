ae9da30a7a900967042fa5130d3fa422501684b5
NiekDoo <77921239+NiekDoo@users.noreply.github.com>
2021-11-05 10:11:37 +0100

    Create databaseconnectie.php

---
+++ databaseconnectie.php
@@ -0,0 +1,10 @@
+  <?php
+// Declareer variabelen
+              $servername = "localhost";
+              $username = "root";
+              $password = "usbw";
+              $database = "aanmelding";
+
+              // Maak verbinding
+              $conn = new mysqli($servername, $username, $password, $database);
+?>
