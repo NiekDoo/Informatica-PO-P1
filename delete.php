c44ba22ec8ecc372e3aec9aa81e845a9f26830ed
NiekDoo <77921239+NiekDoo@users.noreply.github.com>
2021-11-05 20:21:29 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,21 +1,19 @@
 <?php
 require('databaseconnectie.php');
 
-$id = $_GET['id'];
+$id = "'" . $_GET['id'] . "'";
 
-$sql = "DELETE FROM aanmeldingen WHERE email = $id"; 
+$sql = "DELETE FROM aanmelding WHERE email=$id"; 
 
-mysqli_query($conn, $sql); 
 
-/*
 if (mysqli_query($conn, $sql)) {
     mysqli_close($conn);
-    header('Location: index.php'); //If book.php is your main page where you list your all records
+    echo "Succesvol verwijderd. ";
     exit;
 } else {
-    echo "Error deleting record";
+    echo "Er is iets misgegaan, probeer het opnieuw. " . $conn->error;
 }
-*/
 
+mysqli_close($conn);
 
 ?>
