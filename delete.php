305ee28e1483a79f00c0c8b4e7a64bd42e99dd87
NiekDoo <77921239+NiekDoo@users.noreply.github.com>
2021-11-05 21:12:48 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -5,10 +5,11 @@ $id = "'" . $_GET['id'] . "'";
 
 $sql = "DELETE FROM aanmelding WHERE email=$id"; 
 
-
+// Uitvoeren van de query
 if (mysqli_query($conn, $sql)) {
     mysqli_close($conn);
     echo "Succesvol verwijderd. ";
+    header('Location: index.php');
     exit;
 } else {
     echo "Er is iets misgegaan, probeer het opnieuw. " . $conn->error;
