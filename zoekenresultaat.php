be4947e1226a37b1c81a89a907e13691cbad5958
NiekDoo <77921239+NiekDoo@users.noreply.github.com>
2021-11-06 16:00:15 +0100

    Update zoekenresultaat.php

--- zoekenresultaat.php
+++ zoekenresultaat.php
@@ -7,10 +7,10 @@
     <title>Aanmeldingen</title>
   </head>
   <body>
-    <input class="btn btn-outline-primary" type="button" value="Nog een keer zoeken" onClick="document.location.href='/index.php'" />
+    <h1>Zoeken</h1>
+    <input class="btn btn-outline-primary" id="nogeenkeerzoeken" type="button" value="Nog een keer zoeken" onClick="document.location.href='/index.php'" />
+    <br>
     <table class="table table-striped table-bordered">
-    <br>
-    <br>
           <tr>
             <th>Naam</th>
             <th>Email</th>
@@ -27,42 +27,12 @@
           die("Connectie mislukt: " . $conn->connect_error);
       }
 
-      // zoekformulier verwerken
+      // Zoekformulier verwerken
       if (isset($_POST["query"])) {
 
           $query = $_POST['query'];
 
-          /*
-          // makes sure nobody uses SQL injection
-          $query = mysql_real_escape_string($query);
-        
-        
-          $raw_results = mysql_query("SELECT * FROM aanmeldingen WHERE (naam LIKE '%".$query."%') OR (email LIKE '%".$query."%') OR (aanwezig LIKE '%".$query."%') OR (aantal LIKE '%".$query."%') ");
-          echo $raw_results; 
-          echo "Hi"; 
-          */
-
-
-          /*
-          $stmt = $conn->prepare("SELECT * FROM aanmelding WHERE (naam LIKE '%`.$query.`%') OR (email LIKE '%`.$query.`%') OR (aanwezig LIKE '%`.$query.`%') OR (aantal LIKE '%`.$query.`%') ");
-              // hier ga je pas de variabelen toewijzen en de query uitvoeren, dat kan je meer keren doen.
-              
-              
-              $stmt->execute();
-              echo $stmt; 
-              $stmt->close(); 
-          */
-
-
-          // Laat tabel zien
-          //$sql = "SELECT naam, email, aanwezig, aantal FROM aanmelding WHERE (naam LIKE '%".$query."%') OR (email LIKE '%".$query."%') OR (aanwezig LIKE '%".$query."%') OR (aantal LIKE '%".$query."%') ";
-
-          //$stmt = $conn->prepare("INSERT INTO aanmelding (naam, email, aanwezig, aantal) VALUES (?, ?, ?, ?)");
-          //$stmt -> bind_param("ssss", $naam, $email, $aanwezig, $aantal);
-
-
-
-
+          // Query voorbereiden
           $sql = "SELECT naam, email, aanwezig, aantal FROM aanmelding WHERE naam LIKE CONCAT('%',?,'%') OR email LIKE CONCAT('%',?,'%')  OR aanwezig LIKE CONCAT('%',?,'%') OR aantal LIKE CONCAT('%',?,'%')";
           $stmt = $conn->prepare($sql);
           $stmt -> bind_param("ssss", $naam, $email, $aanwezig, $aanwezig);
@@ -79,21 +49,9 @@
 
           $stmt->close(); 
 
-          /*
-          $sql = "SELECT * FROM users WHERE id=?"; // SQL with parameters
-          $stmt = $conn->prepare($sql); 
-          $stmt->bind_param("i", $id);
-          $stmt->execute();
-          $result = $stmt->get_result(); // get the mysqli result
-          $user = $result->fetch_assoc(); // fetch data   
-          */
-
-
-
-          //$result = $conn->query($stmt);
-
+          // Laat tabel zien
           if ($result->num_rows > 0) {
-            // output data of each row
+            // Iedere keer een nieuwe rij maken
             while($row = $result->fetch_assoc()) {
               echo "<tr><td>" . $row["naam"]. "</td>
                     <td>" . $row["email"] . "</td>
@@ -106,28 +64,14 @@
             echo "<p>Er zijn geen overeenkomsten gevonden<p>";
           }
 
-
-
-
-
-          
-
-          //Sluit verbinding
-          $conn->close();
-
-
-
-
-
-
       } else { 
           echo "error, probeer het opnieuw";
       }
 
+      // Sluit verbinding
+      $conn->close();
 
       ?>
     </table>
-    
-
   </body>
 </html>
